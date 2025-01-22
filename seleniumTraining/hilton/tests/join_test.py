913206c012f102f8030c42ec5b8befbe22487337
saraspitz <afteravodot@gmail.com>
2025-01-22 15:01:37 +0200

    my first commit

---
+++ seleniumTraining/hilton/tests/join_test.py
@@ -0,0 +1,19 @@
+import time
+
+from selenium.webdriver.common.by import By
+
+from seleniumTraining.hilton.pages.join_page import joinPage
+from seleniumTraining.hilton.tests.selenuimBaseHilton import seleniumBaseHilton
+
+
+class join_new_user():
+    base = seleniumBaseHilton()
+    driver = base.selenium_start()
+    join_page = joinPage(driver)
+
+    driver.get("https://www.hilton.com/en/")
+    time.sleep(3)
+    free_join = driver.find_element(By.LINK_TEXT,"Join for Free")
+    free_join.click()
+
+    join_page.join_new_user("sara","spitz","02-9999999", "02-9999999")
